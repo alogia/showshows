92f2768f1756a86ba605e188ad023ee26e57c36b
Tyler Thomas <alogia@gmail.com>
2017-05-12 12:54:47 -0500

    Object finally commit to database, but create duplicates of episodes.

--- watchseries.lisp
+++ watchseries.lisp
@@ -1,4 +1,5 @@
 (in-package :showshows)
+(clsql:file-enable-sql-reader-syntax)
 
 ;;; Functions to parse the main Watchseries website
 
@@ -6,17 +7,20 @@
 ;; Functions for show listing page
 ;; ---------------------------------------------------------------
 
+
 (defun init-show (name url)
   (parse-show name url (get-dom url)))
 
 (defun parse-show (name url node)
-  (let ((show-id (car (clsql:update-records-from-instance (make-instance 'show :name name :url url)))))
-	(parse-seasons node show-id)))
+  (let ((show-id (caar (clsql:select [id] :from [show] :where [= [url] url]))))
+    (if show-id
+	(parse-seasons node show-id)
+	(parse-seasons node (car (clsql:update-records-from-instance (make-instance 'show :name name :url url)))))))
 
 (defun parse-seasons (node show-id)
   "Call this method on the main seasons page for a show"
-    (mapcar #'(lambda (n) (clsql:update-records-from-instance (process-season n show-id)))
-	    (collect-nodes node (elements ("itemprop" . "season"))))))
+    (mapcar #'(lambda (n) (process-season n show-id))
+	    (collect-nodes node (elements ("itemprop" . "season")))))
 
 (defun process-season (node show-id)
   "Internal function to parse the season nodes"
@@ -24,16 +28,21 @@
 	 (num (parse-integer (car (ppcre:all-matches-as-strings "[0-9]+$" (html5-parser:element-attribute listing "id")))))
 	 (a (find-first-node node 'html-recurse-p (types "a")))
 	 (url (html5-parser:element-attribute a "href")))
-    (let ((season-id (make-instance 'season
-		   :url url
-		   :show-id show-id
-		   :num num)))
-    (parse-episodes node show-id season-id))))
+    (let ((season-id (caar (clsql:select [id] :from [season] :where [= [num] num]))))
+      (if season-id
+	  (parse-episodes node show-id season-id)
+	  (parse-episodes node show-id (car (clsql:update-records-from-instance (make-instance 'season
+										 :url url
+										 :show-id show-id
+										 :num num))))))))
+	  
 
 (defun parse-episodes (node show-id season-id)
   "Internal function to parse season nodes"
-  (mapcar (lambda (s) (clsql:update-records-from-instance (process-episode s show-id season-id)))
-	  (collect-nodes node (elements ("itemprop" . "episode")))))
+  (let ((new-eps (mapcar (lambda (s) (process-episode s show-id season-id))
+	  (collect-nodes node (elements ("itemprop" . "episode"))))))
+    (dolist (e new-eps) (if e
+			    (clsql:update-records-from-instance e)))))
 
 (defun process-episode (node show-id season-id)
   "Internal function to parse episode nodes"
@@ -42,14 +51,16 @@
     (let
 	 ((url (html5-parser:element-attribute a "href"))
 	  (name (html5-parser:node-first-child (find-first-node a #'html-recurse-p (elements ("itemprop" . "name")))))
-	  (date (html5-parser:node-first-child (find-first-node a #'html-recurse-p (elements ("itemprop" . "datepublished"))))))
-      (make-instance 'episode
+	  (date (html5-parser:node-first-child (find-first-node a #'html-recurse-p (elements ("itemprop" . "datepublished")))))
+	  (ep-url (caar (clsql:select [url] :from [episode] :where [= [num] (parse-integer num)]))))
+      (if (not (equalp url ep-url))
+	  (make-instance 'episode
 		     :show-id show-id
 		     :season-id season-id
-		     :num num
+		     :num (parse-integer num)
 		     :name (if name (html5-parser:node-value name) "Unknown")
 		     :date (if date (html5-parser:node-value date) "Unknown")
-		     :url url)))) ; Init host links to nil, and fill only upon request.
+		     :url url))))) ; Init host links to nil, and fill only upon request.
 
 
 ;; Functions for host listing
@@ -70,4 +81,4 @@
 (defun get-links (node)
   (cdr (mapcar (lambda (n) (html5-parser:element-attribute n "href"))
 	  (collect-nodes node (elements ("target" . "_blank") ("class" . "buttonLink"))))))
-2
+
